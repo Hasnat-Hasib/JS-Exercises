722e280544db0d4182c16f1e2ba1319f281636a6
Hasnat Hasib <hasib8nh@gmail.com>
2018-05-21 03:01:55 +0000

    task2

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /* JavaScript code here */
 
     console.log("intrnal js");
+// Task2
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index){
+        sum += value;
+    });
+    return sum;
+}
+
+    console.log(sumArray([1,2,3,4,5]));
+    console.log(sumArray([19,-2,30,-45]));
