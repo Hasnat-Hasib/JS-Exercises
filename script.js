ae73fc22e8467aaf0e455b87ecb61be4f6d4052c
Hasnat Hasib <hasib8nh@gmail.com>
2018-05-23 02:56:39 +0000

    task5a

--- script.js
+++ script.js
@@ -47,3 +47,31 @@ var library = [
         return library[index].readingStatus;
     }
     console.log(getReadingStatus(2));
+    
+    
+    //Task5
+    
+    var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+    function addItem(newItem){
+        cart.push(newItem);
+        console.log(cart)
+    }
+    
+    var newItem={name: "Watch", price:64, quantity:1};
+    addItem(newItem);
