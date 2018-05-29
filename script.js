ef63be9ff8f8726a269e3a2ea1e52e94d664f3c8
Tamzida Azad <1421064@iub.edu.bd>
2018-05-29 23:51:56 +0600

    Updated Assignment Task-5

--- script.js
+++ script.js
@@ -52,26 +52,80 @@ var library = [
     //Task5
     
     var cart = [ 
-   {
-       name: 'Shoes',
-       price: 560,
-       quantity: 4
-   },
-   {
-       name: 'Regular Tees',
-       price: 455.50,
-       quantity: 6
-   },
-   {
-       name: 'Socks',
-       price: 65.99,
-       quantity: 2
-   }];
+    {
+      name: 'Shoes',
+      price: 560,
+      quantity: 4
+    },
+    {
+      name: 'Regular Tees',
+      price: 655.50,
+      quantity: 6
+    },
+    {
+      name: 'Socks',
+      price: 65.99,
+      quantity: 2
+    }];
 
-    function addItem(newItem){
-        cart.push(newItem);
-        console.log(cart)
+//(a) Adding new item
+
+function addItem( item ){
+  cart.push(item);
+  return cart;
+}
+
+var newItem = {
+  name: 'KK Green Tea',
+  price: 120,
+  quantity: 1
+};
+
+console.log("\n\nNew Item added: \n\n");
+console.log(addItem(newItem));
+
+//(b) sort by price
+function itemByPrice(){
+ var init = [];
+ for(var i=0; i<cart.length; i++){
+   for(var j=i; j<cart.length-1; j++){
+     if(cart[j].price<cart[j+1].price){
+       init=cart[j];
+       cart[j]=cart[j+1];
+       cart[j+1]=init;
+     }
+   }
+ }
+ cart.sort(function(a, b){
+   return (b.price)-(a.price);
+ });
+ return cart;
+}
+
+console.log("\n\nItems are sorted by price in descending order: \n\n");
+console.log(itemByPrice());
+
+  //find by name
+  function findByName( name){
+    var matches=[];
+    var temp1=name.toUpperCase();
+    var temp2;
+    for(var i= 0; i < cart.length; i++){
+      temp2=cart[i].name.toUpperCase();
+      if(temp2.search(temp1)!= -1){
+        matches.push(cart[i]);
+      }
     }
-    
-    var newItem={name: "Watch", price:64, quantity:1};
-    addItem(newItem);
+
+    return matches;
+  }
+  console.log(findByName("Socks"));
+
+//Total Cost
+var sum=0;
+for(var i= 0; i < cart.length; i++){
+  tm=(cart[i].price)*(cart[i].quantity);
+  sum+=tm;
+}
+console.log("\n\nTotal Amount: \n\n");
+console.log(sum); 
